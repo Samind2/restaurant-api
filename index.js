93ef40a561551cc09549bae48fb55261a358c01a
Samind2 <suphaneerangsirat@gmail.com>
2024-07-08 17:04:24 +0700

    Update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-
+const PORT = 5000;
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('<h1>Hello Restaurant API</h1>');
+app.get("/", (req,res) => {
+    res.send("<h1>Hello Restaurant API</h1>");
 });
 
 app.listen(PORT, () => {
-    console.log('Listening to http:/localhost:'+PORT);
+    console.log("Listening to http://localhost:"+PORT);
 })
