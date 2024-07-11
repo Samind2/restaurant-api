de6bfbe288d24644dbe1a26bb3d068ea12c54676
Samind2 <suphaneerangsirat@gmail.com>
2024-07-11 14:24:55 +0700

    update11/07/67

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
 
 module.exports = {
-  HOST: "ep-gentle-bar-a11v5ltt-pooler.ap-southeast-1.aws.neon.tech",
-  USER: "default",
-  PASSWORD: "5iMSa3hfRlGq",
-  DB: "verceldb",
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
   dialect: "postgres",
   pool: {
     max: 5,
