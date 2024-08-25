52a746387ce5b33a08a49bc7c2c83e1b004f0d50
Samind2 <suphaneerangsirat@gmail.com>
2024-08-25 20:37:29 +0700

    Up

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 const { verifySignUp } = require("../middlewares");
 
+router.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
+
 // ลงทะเบียนผู้ใช้
 router.post(
   "/signup",
